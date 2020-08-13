320e588a831c600ddc26105498dfb874e0828a3c
VladKrauchuk <66268773+VladKrauchuk@users.noreply.github.com>
2020-08-13 10:56:28 +0300

    1 day

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <header>
+        <h1>Кошелек</h1>
+        <h2>Калькулятор расходов</h2>
+    </header>
+
+    <main>
+        <div className="container">
+            <section className="total">
+                <header className="total__header">
+                    <h3>Баланс</h3>
+                    <p className="total__balance">0 ₽</p>
+                </header>
+                <div className="total__main">
+                    <div className="total__main-item total__income">
+                        <h4>Доходы</h4>
+                        <p className="total__money total__money-income">
+                            +0 ₽
+                        </p>
+                    </div>
+                    <div className="total__main-item total__expenses">
+                        <h4>Расходы</h4>
+                        <p className="total__money total__money-expenses">
+                            -0 ₽
+                        </p>
+                    </div>
+                </div>
+            </section>
+
+            <section className="history">
+                <h3>История расходов</h3>
+                <ul className="history__list">
+                    <li className="history__item history__item-plus">Получил зарплату
+                        <span className="history__money">+30000 ₽</span>
+                        <button className="history__delete">x</button>
+                    </li>
+                    <li className="history__item  history__item-minus">Отдал долг
+                        <span className="history__money">-10000 ₽</span>
+                        <button className="history__delete">x</button>
+                    </li>
+                </ul>
+            </section>
+            
+            <section className="operation">
+                <h3>Новая операция</h3>
+                <form id="form">
+                    <label>
+                        <input type="text" className="operation__fields operation__name" placeholder="Наименование операции"/>
+                    </label>
+                    <label>
+                        <input type="number" className="operation__fields operation__amount" placeholder="Введите сумму"/>
+                    </label>
+                    <div className="operation__btns">
+                        <button type="submit" className="operation__btn operation__btn-subtract">РАСХОД</button>
+                        <button type="submit" className="operation__btn operation__btn-add">ДОХОД</button>
+                    </div>
+                </form>
+            </section>
+        </div>
+    </main>
+    </>
   );
 }
 
